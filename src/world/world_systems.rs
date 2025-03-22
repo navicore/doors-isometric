723e9266956b39046749d7266808362937457ce2
Ed Sweeney <ed@onextent.com>
2025-03-22 06:56:42 -0700

    room render experiment 1

--- src/world/world_systems.rs
+++ src/world/world_systems.rs
@@ -1,8 +1,5 @@
 use crate::{floorplan::FloorPlanEvent, state::GameState};
-use bevy::{
-    color::palettes::tailwind::{GRAY_600, RED_500},
-    prelude::*,
-};
+use bevy::{color::palettes::tailwind::GRAY_600, prelude::*};
 use petgraph::prelude::*;
 
 use super::world_component::CurrentFloorPlan;
@@ -55,42 +52,59 @@ pub fn spawn_world(
     debug!("Spawning world");
     if let Some(floorplan) = &current_floorplan.floorplan {
         // Visualize Rooms
-        for node_index in floorplan.graph.node_indices() {
-            if let Some(room) = floorplan.graph.node_weight(node_index) {
-                let position = calculate_room_position(node_index);
-
-                let shape = meshes.add(Cuboid::new(4.0, 1.5, 4.0));
-                let mat = materials.add(Color::from(GRAY_600));
-                commands.spawn((
-                    Mesh3d(shape),
-                    MeshMaterial3d(mat),
-                    Transform::from_translation(position),
-                    room.clone(),
-                ));
+        // for node_index in floorplan.graph.node_indices() {
+        //     if let Some(room) = floorplan.graph.node_weight(node_index) {
+        //         let position = calculate_room_position(node_index);
+        //
+        //         let shape = meshes.add(Cuboid::new(4.0, 1.5, 4.0));
+        //         let mat = materials.add(Color::from(GRAY_600));
+        //         commands.spawn((
+        //             Mesh3d(shape),
+        //             MeshMaterial3d(mat),
+        //             Transform::from_translation(position),
+        //             room.clone(),
+        //         ));
+        //     }
+        // }
+        if let Some(current_room) = &current_floorplan.you_are_here {
+            if let Ok(entries) = &floorplan.get_doors_and_connected_rooms(&current_room.id) {
+                for (_door, room) in entries.iter() {
+                    if let Ok(node_index) = floorplan.get_room_idx_by_id(&room.id) {
+                        let position = calculate_room_position(node_index);
+                        let shape = meshes.add(Cuboid::new(4.0, 1.5, 4.0));
+                        let mat = materials.add(Color::from(GRAY_600));
+                        commands.spawn((
+                            Mesh3d(shape),
+                            MeshMaterial3d(mat),
+                            Transform::from_translation(position),
+                            current_room.clone(),
+                        ));
+                    }
+                }
             }
         }
 
         // Visualize Doors (Edges)
-        for edge in floorplan.graph.edge_references() {
-            let source_pos = calculate_room_position(edge.source());
-            let target_pos = calculate_room_position(edge.target());
-
-            let door_pos = (source_pos + target_pos) / 2.0;
-            let direction = target_pos - source_pos;
-
-            let shape = meshes.add(Cuboid::new(1.0, 1.5, 0.2));
-            let mat = materials.add(Color::from(RED_500));
-            commands.spawn((
-                Mesh3d(shape),
-                MeshMaterial3d(mat),
-                Transform {
-                    translation: door_pos,
-                    rotation: Quat::from_rotation_y(direction.z.atan2(direction.x)),
-                    ..default()
-                },
-                edge.weight().clone(),
-            ));
-        }
+        // for edge in floorplan.graph.edge_references() {
+        //     let source_pos = calculate_room_position(edge.source());
+        //     let target_pos = calculate_room_position(edge.target());
+        //
+        //     let door_pos = (source_pos + target_pos) / 2.0;
+        //     let direction = target_pos - source_pos;
+        //
+        //     let shape = meshes.add(Cuboid::new(1.0, 1.5, 0.2));
+        //     let mat = materials.add(Color::from(RED_500));
+        //     commands.spawn((
+        //         Mesh3d(shape),
+        //         MeshMaterial3d(mat),
+        //         Transform {
+        //             translation: door_pos,
+        //             rotation: Quat::from_rotation_y(direction.z.atan2(direction.x)),
+        //             ..default()
+        //         },
+        //         edge.weight().clone(),
+        //     ));
+        // }
 
         next_state.set(GameState::InGame);
     }
