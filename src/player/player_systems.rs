61033e41360e700d7853e4f044f14385f5dfda40
Ed Sweeney <ed@onextent.com>
2025-03-23 08:32:30 -0700

    player renders and has physics but does not stop

--- src/player/player_systems.rs
+++ src/player/player_systems.rs
@@ -1,63 +1,90 @@
+use super::player_component::{
+    Action, Grounded, Player, PlayerBundle, PlayerDirection, PlayerState, PLAYER_SHAPE_X,
+    PLAYER_SHAPE_Y, PLAYER_SHAPE_Z,
+};
 use avian3d::prelude::*;
 use bevy::{color::palettes::tailwind::BLUE_600, prelude::*};
-use leafwing_input_manager::{
-    prelude::{ActionState, InputMap},
-    InputManagerBundle,
-};
-
-use super::player_component::{Action, Player};
+use leafwing_input_manager::prelude::ActionState;
 
 pub fn spawn_player(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    let player_shape = meshes.add(Cuboid::new(0.5, 1.0, 0.5)); // Player size
+    // TODO integrate these in bundle so they can change
+    let player_shape = meshes.add(Cuboid::new(PLAYER_SHAPE_X, PLAYER_SHAPE_Y, PLAYER_SHAPE_Z)); // Player size
     let player_material = materials.add(Color::from(BLUE_600));
-
-    let input_map = InputMap::new([
-        (Action::MoveForward, KeyCode::ArrowUp),
-        (Action::MoveBackward, KeyCode::ArrowDown),
-        (Action::MoveLeft, KeyCode::ArrowLeft),
-        (Action::MoveRight, KeyCode::ArrowRight),
-        (Action::Jump, KeyCode::Space),
-        (Action::Enter, KeyCode::Enter),
-    ]);
 
     commands.spawn((
         Mesh3d(player_shape),
         MeshMaterial3d(player_material),
-        Transform::from_translation(Vec3::new(0.0, 5.0, 0.0)), // Start above the floor
-        RigidBody::Dynamic,              // Dynamic body for physics interactions
-        Collider::cuboid(0.5, 1.0, 0.5), // Collider matching the player size
-        ExternalForce::default(),
-        Player::default(),
-        InputManagerBundle::with_map(input_map),
+        PlayerBundle::new(),
     ));
 }
 
-pub fn player_movement(mut query: Query<(&mut ExternalForce, &ActionState<Action>), With<Player>>) {
-    if let Ok((mut force, action_state)) = query.get_single_mut() {
+pub fn player_movement(
+    mut query: Query<
+        (
+            &mut ExternalForce,
+            &Grounded,
+            &ActionState<Action>,
+            &mut Player,
+        ),
+        With<Player>,
+    >,
+) {
+    if let Ok((mut force, grounded, action_state, mut player)) = query.get_single_mut() {
+        if !grounded.0 {
+            debug!("Player is not grounded");
+        }
+
         let mut direction = Vec3::ZERO;
 
-        if action_state.pressed(&Action::MoveForward) {
+        let mut pressed = if action_state.pressed(&Action::MoveForward) {
             direction.z += 1.0;
-        }
-        if action_state.pressed(&Action::MoveBackward) {
+            player.direction = PlayerDirection::Up;
+            player.state = PlayerState::Walk;
+            true
+        } else {
+            false
+        };
+
+        pressed = if action_state.pressed(&Action::MoveBackward) {
             direction.z -= 1.0;
-        }
-        if action_state.pressed(&Action::MoveLeft) {
+            player.direction = PlayerDirection::Down;
+            player.state = PlayerState::Walk;
+            true
+        } else {
+            pressed
+        };
+
+        pressed = if action_state.pressed(&Action::MoveLeft) {
             direction.x += 1.0;
-        }
-        if action_state.pressed(&Action::MoveRight) {
+            player.direction = PlayerDirection::Left;
+            player.state = PlayerState::Walk;
+            true
+        } else {
+            pressed
+        };
+
+        pressed = if action_state.pressed(&Action::MoveRight) {
             direction.x -= 1.0;
-        }
+            player.direction = PlayerDirection::Right;
+            player.state = PlayerState::Walk;
+            true
+        } else {
+            pressed
+        };
 
         // Normalize direction and apply force
         if direction != Vec3::ZERO {
             direction = direction.normalize();
         }
 
-        force.apply_force(direction * 1.0); // Adjust force magnitude as needed
+        force.apply_force(direction * player.walk_speed); // Adjust force magnitude as needed
+
+        if !pressed {
+            player.state = PlayerState::Stand;
+        }
     }
 }
