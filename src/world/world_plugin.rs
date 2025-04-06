3c1e2729f89eb0c12bedcb94c38d40f23c5ee1fd
Ed Sweeney <ed@onextent.com>
2025-04-05 17:24:09 -0700

    lua config works

--- src/world/world_plugin.rs
+++ src/world/world_plugin.rs
@@ -1,16 +1,36 @@
-use bevy::prelude::*;
-
-use crate::state::GameState;
-
 use super::{
     world_component::{CurrentFloorPlan, WorldConfig, WorldPlugin},
     world_systems::{handle_floor_plan_event, platform_transition_system, spawn_world},
 };
+use crate::state::GameState;
+use bevy::prelude::*;
+
+fn load_world_config_from_lua() -> WorldConfig {
+    use rlua::{Lua, Table};
+
+    let lua = Lua::new();
+    std::fs::read_to_string("assets/config.lua").map_or_else(
+        |_| WorldConfig::default(),
+        |script| {
+            lua.load(&script)
+                .eval::<Table>()
+                .and_then(|config_table| config_table.get::<_, Table>("world_config"))
+                .map(|config_table| WorldConfig {
+                    room_x: config_table.get("room_x").unwrap_or(4.0),
+                    room_y: config_table.get("room_y").unwrap_or(4.0),
+                    room_z: config_table.get("room_z").unwrap_or(4.0),
+                    placeholder_y: config_table.get("placeholder_y").unwrap_or(0.1),
+                    exit_room_y: config_table.get("exit_room_y").unwrap_or(4000.0),
+                })
+                .unwrap_or_default()
+        },
+    )
+}
 
 impl Plugin for WorldPlugin {
     fn build(&self, app: &mut App) {
         app.insert_resource(CurrentFloorPlan::default())
-            .insert_resource(WorldConfig::default())
+            .insert_resource(load_world_config_from_lua())
             .add_systems(
                 Update,
                 handle_floor_plan_event.run_if(in_state(GameState::InGame)),
